2de9805d58b336edc3aaacfe9751efb7c8815033
Clemens Punzengruber <clemens.punzengruber@rise-world.com>
2017-07-13 18:16:28 +0200

    new version

--- contracts/HoffDapp.sol
+++ contracts/HoffDapp.sol
@@ -11,34 +11,45 @@ contract HoffDapp {
     string name;
   }
 
-  mapping(address=>bool) public admins;
-  mapping(address=>User) public users;
-  mapping(string=>address) registeredNames;
+  struct Menu {
+    string id;
+    string name;
+    int priceInCents;
+  }
 
+  event MenuConsumed(address buyer, string menuId, int priceInCents);
+
+
+  mapping(address=>bool) admins;
+  mapping(address=>User) users;
+  mapping(string=>address) registeredNames;
+  mapping(int=>address) userAddresses;
+  int userCount = 0;
+
+  mapping(string=>Menu) menus;
+  mapping(int=>string) menuIds;
+  int menuCount = 0;
+
+  mapping(address=>int) balances;
 
   function addAdmin(address addr) {
     admins[addr] = true;
   }
+
   function isAdmin(address addr) returns (bool isAdmin) {
     return admins[addr] == true;
   }
 
-  function register(string name) {
-    require(users[msg.sender].addr == 0);
+  function addUser(address addr, string name) onlyAdmin {
+    require(users[addr].addr == 0);
     require(registeredNames[name] == 0);
 
-    users[msg.sender].addr = msg.sender;
-    users[msg.sender].name = name;
-    users[msg.sender].state = UserState.INACTIVE;
-    registeredNames[name] = msg.sender;
-  }
-
-
-  function activateUser(address addr) onlyAdmin {
-    require(users[addr].addr != 0);
-    require(users[addr].state == UserState.INACTIVE);
-
+    users[addr].addr = addr;
+    users[addr].name = name;
     users[addr].state = UserState.ACTIVE;
+    registeredNames[name] = addr;
+    userAddresses[userCount] = addr;
+    userCount++;
   }
 
   function getUserName(address addr) returns (string name) {
@@ -49,15 +60,32 @@ contract HoffDapp {
     return (users[addr].state == UserState.ACTIVE);
   }
 
-  //function getUser(address addr) returns (String name, UserState state) {
-  //  return users[addr].name, users[addr].state);
-  //}
+  function getUser(address addr) returns (string name, bool active, int balance) {
+    return (users[addr].name, users[addr].state==UserState.ACTIVE, balances[addr]);
+  }
 
+  function addMenu(string id, string name, int priceInCents) onlyAdmin {
+    if(bytes(menus[id].id).length == 0) {
+      menuIds[menuCount] = id;
+      menuCount++;
+    }
+    menus[id].id = id;
+    menus[id].name = name;
+    menus[id].priceInCents = priceInCents;
+  }
+
+  function consumeMenu(string id) {
+    require(users[msg.sender].state==UserState.ACTIVE);
+    balances[msg.sender]+=menus[id].priceInCents;
+    MenuConsumed(msg.sender, id, menus[id].priceInCents);
+  }
 
   //=========== SECTION: Modifier ===========
   modifier onlyAdmin() {
     require(admins[msg.sender]);
     _;
   }
+
+
 
 }
